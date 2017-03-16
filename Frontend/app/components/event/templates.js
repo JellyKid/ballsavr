4c6aef0beb519c4749717e083778b949c5b76996
Jacob Sommerville <jsommerville@bayportstatebank.com>
2017-03-16 09:34:37 -0400

    Adding _id to event template

--- Frontend/app/components/event/templates.js
+++ Frontend/app/components/event/templates.js
@@ -1,4 +1,5 @@
 export const blankEvent = {
+  _id: null,
   title: "",
   subtitle: "",
   type: "tournament",
@@ -9,7 +10,7 @@ export const blankEvent = {
 };
 
 export const blankRound = {
-  id: null,
+  _id: null,
   name: "",
   prev: null,
   next: null,
