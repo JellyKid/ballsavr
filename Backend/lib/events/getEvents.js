84cd78de145d01589ac156f18c8126c8690c1dc0
Jacob Sommerville <jsommerville@bayportstatebank.com>
2017-04-12 13:33:10 -0400

    Since event model now includes rounds, populate rounds for get request. This will probably make getRoundsByEventID obsolete.

--- Backend/lib/events/getEvents.js
+++ Backend/lib/events/getEvents.js
@@ -2,14 +2,31 @@ const Event = require('../../db/models/event');
 
 function getEvents(req, res, next) {
   res.locals.events = [];
-  return Event.find(
-    {enabled: true},
+  return Event
+  .find({enabled: true})
+  .populate({
+    path: 'rounds',
+    model: 'round',
+    populate: [
+      {
+        path: 'players.user',
+        model: 'user',
+        select: 'firstName lastName'
+      },
+      {
+        path: 'tables',
+        model: 'table',
+        select: 'name'
+      }
+    ]
+  })
+  .lean()
+  .exec(
     (err, events) => {
       if(err){return next(err);}
       res.locals.events = events;
       return next();
     }
-  );  
+  );
 }
-
 module.exports = getEvents;
