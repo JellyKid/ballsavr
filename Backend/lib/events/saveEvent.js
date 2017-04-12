0acf78467697727556570d98e59b2373fd939d40
Jacob Sommerville <jsommerville@bayportstatebank.com>
2017-04-12 11:37:18 -0400

    Cleaning up white space

--- Backend/lib/events/saveEvent.js
+++ Backend/lib/events/saveEvent.js
@@ -2,9 +2,8 @@ const path = require('path');
 const Event = require(path.normalize('../../db/models/event'));
 
 function updateEvent(req, res, next) {
-
   const event = new Event(req.body.event);
-  event.isNew = req.body.event._id ? false : true;  
+  event.isNew = req.body.event._id ? false : true;
   return event.save(
     (err, doc) => {
       if(err) {return next(err);}
