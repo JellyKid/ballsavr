3686ca19eaee64aeda82ca5366b38b46ce050ee1
Jacob Sommerville <jsommerville@bayportstatebank.com>
2017-04-12 09:52:18 -0400

    Removing console log

---
+++ Backend/lib/events/updateEventWithRounds.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const Event = require(path.normalize('../../db/models/event'));
+
+function updateEventWithRounds(req, res, next) {
+  if(res.locals.event && res.locals.rounds){  
+    return res.locals.event.update(
+      {rounds: res.locals.rounds},
+      (err, doc) => {
+        if(err){return next(err);}
+        res.locals.event = doc;
+        return next();
+      }
+    );
+  }
+  console.log('updateEventWithRounds: no rounds or events found');
+  return next();
+}
+
+module.exports = updateEventWithRounds;
