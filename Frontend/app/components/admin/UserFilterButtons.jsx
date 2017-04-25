45bce3885481c83e1540c1a596786140b45f8ff7
Jacob Sommerville <jsommerville@bayportstatebank.com>
2017-04-25 09:56:47 -0400

    Changing filter wording

--- Frontend/app/components/admin/UserFilterButtons.jsx
+++ Frontend/app/components/admin/UserFilterButtons.jsx
@@ -39,9 +39,9 @@ export default class UserFilterButtons extends React.Component {
           <MenuItem
             eventKey='not.activated'
             id='user-filter-by-not-activated'
-            onSelect={(key,e) => this.handleSelect('Not-activated', 'meta.activated', false, e)}
-            disabled={this.props.currentFilter === 'Not-activated'}>
-            Not-activated
+            onSelect={(key,e) => this.handleSelect('Not Activated', 'meta.activated', false, e)}
+            disabled={this.props.currentFilter === 'Not Activated'}>
+            Not Activated
           </MenuItem>
         </DropdownButton>
       </ButtonGroup>
